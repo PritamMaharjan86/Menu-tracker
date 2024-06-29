8db0b496fe866923cec96616ff82486b8659cd3e
Pritam Maharjan <primgdev@gmail.com>
2024-06-29 17:15:33 +1000

    update

--- src/App.js
+++ src/App.js
@@ -1,19 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from "react";
+
+
+
 
 function App() {
+
+  const [budget, setBudget] = useState('');
+
+
+  const handleChange = (e) => {
+    setBudget(e.target.value);
+  }
+
+  const handleSubmit = () => {
+    alert(`The name you entered was: ${budget}`)
+  }
+
+
+  const detectKey = (e) => {
+    if (e.key === "Enter") {
+      handleSubmit();
+    }
+  }
+
+
+
   return (
     <div className="App">
       <span>Expense Tracker</span>
+
       <div className="tab">
         <div className="income">
-          <span>Income</span>
+
+          <input
+            type="number"
+            className="data"
+            placeholder="Enter your budget"
+            value={budget}
+            onChange={(e) => handleChange(e)}
+            onKeyUp={detectKey}
+          ></input>
+
+
+          <span>Budget:{budget}</span>
+
 
         </div>
-        <div className="expenses">
-          <span>Expenses</span>
 
-        </div>
       </div>
     </div>
   );
