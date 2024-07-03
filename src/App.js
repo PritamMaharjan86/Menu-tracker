7bf1bc21fc1af73c095269447e5f941a4d78c269
Pritam Maharjan <primgdev@gmail.com>
2024-07-03 19:36:31 +1000

    feature/3/cru

--- src/App.js
+++ src/App.js
@@ -9,34 +9,48 @@ export default function App() {
 
   const [name, setName] = useState('');
   const [number, setNumber] = useState('');
+  const [client, setClient] = useState('');
   const [items, setItems] = useState([]);
   const [field, setField] = useState(true);
   const [isDisabled, setIsDisabled] = useState(true);
+  const [saveDisabled, setSavedisabled] = useState(false);
+  const [editDisabled, setEditdisabled] = useState(true);
 
 
 
 
 
-  const handleChange = (e) => {
+  const handleName = (e) => {
     const name = e.target.value;
     if (/^[a-zA-Z]?$/.test(name)) { // Only allow digits
       setName(name);
     }
+  };
 
+  const handleNumber = (e) => {
     const number = e.target.value;
     if (/^\d{0,3}$/.test(number)) {
       setNumber(number);
     }
+  };
 
+  const handleClient = (e) => {
+    const client = e.target.value;
+    if (/^[a-zA-Z]*$/.test(client)) {
+      setClient(client);
+    }
   };
 
   const handleAddItem = () => {
     setField(false);
     setIsDisabled(false);
+    setSavedisabled(true);
+    setEditdisabled(false);
+
 
     const newItem = {
       id: items.length + 1, // You can use a more robust ID generation method
-      name: (name) + (number * 2) // Example name generation
+      name: (client) + " " + (name) + '/' + (number) // Example name generation
     };
     setItems([...items, newItem]);
   };
@@ -45,13 +59,21 @@ export default function App() {
   const handleEdit = () => {
     setField(true);
     setIsDisabled(true);
+    setSavedisabled(false);
+    setEditdisabled(true);
+    
+    setItems(prevItems => prevItems.slice(0, -1));
   }
 
 
-
-
-
-
+  const handleAdd = () => {
+    setSavedisabled(false);
+    setField(true);
+    setEditdisabled(true);
+    setClient('');
+    setName('');
+    setNumber('');
+  }
 
 
   return (
@@ -62,27 +84,39 @@ export default function App() {
       <div className="display">
 
         {field && (<input
+
+          className="data"
+          placeholder="Enter client number"
+          value={client}
+          onChange={(e) => handleClient(e)}
+        />)}
+        {field && (<input
           type="text"
           className="data"
           placeholder="Enter menu name"
           value={name}
-          onChange={(e) => handleChange(e)}
+          onChange={(e) => handleName(e)}
         />)}
         {field && (<input
           type="number"
           className="data"
           placeholder="Enter menu number"
           value={number}
-          onChange={(e) => handleChange(e)}
+          onChange={(e) => handleNumber(e)}
         />)}
 
 
+
         <Box sx={{ display: 'flex', gap: 9, flexWrap: 'wrap' }}>
-          <Button onClick={handleAddItem}>Save</Button>
+          <Button disabled={saveDisabled} onClick={handleAddItem}>Save</Button>
         </Box>
 
         <Box sx={{ display: 'flex', gap: 9, flexWrap: 'wrap' }}>
-          <Button disabled={isDisabled} onClick={handleEdit}>Edit</Button>
+          <Button disabled={editDisabled} onClick={handleEdit}>Edit</Button>
+        </Box>
+
+        <Box sx={{ display: 'flex', gap: 9, flexWrap: 'wrap' }}>
+          <Button disabled={isDisabled} onClick={handleAdd}>Add</Button>
         </Box>
 
       </div>
@@ -91,15 +125,22 @@ export default function App() {
         <div>
           <span>Scones</span>
           <div>
-        {items.map(item => (
-          <li key={item.id}>{item.name}</li>
-        ))}
-      </div>
+
+
+            {items.map(item => (
+              <li key={item.id}>{item.name}</li>
+            ))}
+          </div>
         </div>
-        <span>Platters</span>
-        <span>Fruits</span>
-        <span>Slices</span>
-        <span>Meatballs</span>
+
+        <div>
+          <span>Savoury</span>
+          <div>
+            {items.map(item => (
+              <li key={item.id}>{item.name}</li>
+            ))}
+          </div>
+        </div>
 
       </div>
 
