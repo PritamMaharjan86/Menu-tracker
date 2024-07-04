ebbb2d37895a8c450e344ce17aa4ca6084e8ffad
Pritam Maharjan <primgdev@gmail.com>
2024-07-04 12:22:36 +1000

    feature/3/added-menu-category

--- src/App.js
+++ src/App.js
@@ -47,13 +47,47 @@ export default function App() {
     setSavedisabled(true);
     setEditdisabled(false);
 
+    let newItem;
 
-    const newItem = {
-      id: items.length + 1, // You can use a more robust ID generation method
-      name: (client) + " " + (name) + '/' + (number) // Example name generation
-    };
+
+    switch (name) {
+      case "a":
+        newItem = {
+          id: items.length + 1, // You can use a m0ore robust ID generation method
+          name: (client) + (name) + '/' + (number),
+          category: "Category A"
+        };
+        break;
+      case "b":
+        newItem = {
+          id: items.length + 1,
+          name:  (client) + (name) + '/' + (number),
+          category: "Category B"
+        };
+        break;
+      case "c":
+        newItem = {
+          id: items.length + 1,
+          name:  (client) + (name) + '/' + (number),
+          category: "Category C"
+        };
+        break;
+      // Add more cases as needed
+      case "d":
+        newItem = {
+          id: items.length + 1,
+          name:  (client) + (name) + '/' + (number),
+          category: "Category D"
+        };
+        break;
+    }
     setItems([...items, newItem]);
   };
+
+
+  const handleDelete = () => {
+
+  }
 
 
   const handleEdit = () => {
@@ -61,7 +95,7 @@ export default function App() {
     setIsDisabled(true);
     setSavedisabled(false);
     setEditdisabled(true);
-    
+
     setItems(prevItems => prevItems.slice(0, -1));
   }
 
@@ -119,28 +153,116 @@ export default function App() {
           <Button disabled={isDisabled} onClick={handleAdd}>Add</Button>
         </Box>
 
+        <Box sx={{ display: 'flex', gap: 9, flexWrap: 'wrap' }}>
+          <Button onClick={handleDelete}>Delete</Button>
+        </Box>
+
       </div>
 
       <div className='menu'>
-        <div>
-          <span>Scones</span>
+
+      <div>
+          <span className='title'>Club Sandwiches</span>
           <div>
-
-
-            {items.map(item => (
-              <li key={item.id}>{item.name}</li>
-            ))}
+          {items
+              .filter(item => item.category === "Category C" || item.category === "Category D")
+              .map(item => (
+                <li key={item.id}>{item.name}</li>
+              ))}
           </div>
         </div>
 
         <div>
-          <span>Savoury</span>
+          <span className='title'>Ribbon Sandwiches</span>
           <div>
-            {items.map(item => (
-              <li key={item.id}>{item.name}</li>
-            ))}
+          {items
+              .filter(item => item.category === "Category D")
+              .map(item => (
+                <li key={item.id}>{item.name}</li>
+              ))}
           </div>
         </div>
+
+        <div>
+          <span className='title'>Macarons</span>
+          <div>
+          {items
+              .filter(item => item.category === "Category C" || item.category === "Category D")
+              .map(item => (
+                <li key={item.id}>{item.name}</li>
+              ))}
+          </div>
+        </div>
+
+        <div>
+          <span className='title'>Savoury</span>
+          <div>
+          {items
+              .filter(item => item.category === "Category B" || item.category === "Category C")
+              .map(item => (
+                <li key={item.id}>{item.name}</li>
+              ))}
+          </div>
+        </div>
+
+
+        <div>
+          <span className='title'>Scones</span>
+          <div>
+          {items
+              .filter(item => item.category === "Category A")
+              .map(item => (
+                <li key={item.id}>{item.name}</li>
+              ))}
+          </div>
+        </div>
+
+        <div>
+          <span className='title'>Savoury Muffin</span>
+          <div>
+          {items
+              .filter(item => item.category === "Category A")
+              .map(item => (
+                <li key={item.id}>{item.name}</li>
+              ))}
+          </div>
+        </div>
+
+
+        
+        <div>
+          <span className='title'>Fruits</span>
+          <div>
+          {items
+              .filter(item => item.category === "Category D")
+              .map(item => (
+                <li key={item.id}>{item.name}</li>
+              ))}
+          </div>
+        </div>
+
+        <div>
+          <span className='title'>Slice</span>
+          <div>
+          {items
+              .filter(item => item.category === "Category B")
+              .map(item => (
+                <li key={item.id}>{item.name}</li>
+              ))}
+          </div>
+        </div>
+
+        <div>
+          <span className='title'>Meatballs</span>
+          <div>
+          {items
+              .filter(item => item.category === "Category D")
+              .map(item => (
+                <li key={item.id}>{item.name}</li>
+              ))}
+          </div>
+        </div>
+
 
       </div>
 
