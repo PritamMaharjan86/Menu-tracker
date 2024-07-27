ad9f792e9b2f729885474ea9e7bc83855e46b6c2
Pritam Maharjan <primgdev@gmail.com>
2024-07-27 19:23:36 +1000

    feature/3/adding dropdwon menu

--- src/components/input.js
+++ src/components/input.js
@@ -15,33 +15,52 @@ export default function Input({ handleClient, handleName, handleNumber, handlePl
                 onChange={(e) => handleClient(e)}
                 required
             />)}
-            {field && (<input
-                type="text"
-                className="data"
-                placeholder="Enter menu name"
-                value={name}
-                onChange={(e) => handleName(e)}
-                required
-            />)}
-            {field && (<input
-                type="number"
-                className="data"
-                placeholder="Enter menu number"
-                value={number}
-                onChange={(e) => handleNumber(e)}
-                required
-            />)}
+            {field && (
+                <div>
+                    <select
+                        className="data"
+                        value={name}
+                        onChange={(e) => handleName(e)}
+                        required
+                    >
+                        <option value="" disabled>Choose menu</option>
+                        <option value="A">A</option>
+                        <option value="B">B</option>
+                        <option value="C">C</option>
+                        <option value="D">D</option>
+                        <option value="E">E</option>
+                        <option value="ITALIAN">ITALIAN</option>
+                        <option value="GREEK DELUXE">GREEK DELUXE</option>
+                        <option value="GREEK LIGHT">GREEK LIGHT</option>
+                        <option value="ASIAN">ASIAN</option>
+                        <option value="MACEDOINIAN">MACEDOINIAN</option>
+                    </select>
+                </div>
+            )}
 
-            {field && (<input
-                type="text"
-                className="data"
-                placeholder="Enter platter name"
-                value={platter}
-                onChange={(e) => handlePlatter(e)}
-                required
-            />)}
+            {
+                field && (<input
+                    type="number"
+                    className="data"
+                    placeholder="Enter menu number"
+                    value={number}
+                    onChange={(e) => handleNumber(e)}
+                    required
+                />)
+            }
 
-        </div>
+            {
+                field && (<input
+                    type="text"
+                    className="data"
+                    placeholder="Enter platter name"
+                    value={platter}
+                    onChange={(e) => handlePlatter(e)}
+                    required
+                />)
+            }
+
+        </div >
     )
 }
 
