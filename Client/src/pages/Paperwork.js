9a7d94b67399c97c7ff698e73a6a7a27f3afdff9
Pritam Maharjan <primgdev@gmail.com>
2026-07-02 20:22:35 +1000

    update paperwork

--- Client/src/pages/Paperwork.js
+++ Client/src/pages/Paperwork.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Input from "../components/Input";
-import Sandwich from "../components/Order";
+import Order from "../components/Order";
 import Menu from "../components/Menu";
 
 const Paperwork = () => {
@@ -10,18 +10,44 @@ const Paperwork = () => {
     pax: "",
   });
 
-  const calculatedItems = Menu(form.menuName, form.pax);
+  const [orders, setOrders] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); //stop page from reloading after form submit
+
+    if (!form.menuName || !form.pax) return; //to check if the form is left empty then stop function
+
+    const items = Menu(form.menuName, form.pax);
+
+    const newOrder = {
+      clientName: form.clientName,
+      menuName: form.menuName,
+      pax: form.pax,
+      items,
+    };
+
+    setOrders((prev) => [...prev, newOrder]);
+
+    setForm({
+      clientName: "",
+      menuName: "",
+      pax: "",
+    });
+  };
 
   return (
     <div>
-      <Input form={form} setForm={setForm} />
+      <Input form={form} setForm={setForm} handleSubmit={handleSubmit} />
 
-      <Sandwich
-        clientName={form.clientName}
-        menuName={form.menuName}
-        pax={form.pax}
-        items={calculatedItems}
-      />
+      {orders.map((order, index) => (
+        <Order
+          key={index}
+          clientName={order.clientName}
+          menuName={order.menuName}
+          pax={order.pax}
+          items={order.items}
+        />
+      ))}
     </div>
   );
 };
